94fbfd4f5ad674b73ba001a3544b56d526b2d9a5
i-X-ce <iceci2804@gmail.com>
2025-10-11 02:21:09 +0900

    ✨ 型を定義

---
+++ src/lib/model/CodeDataModel.ts
@@ -0,0 +1,22 @@
+import PokeVersion from "./pokeVersion";
+
+export interface CodeDataModel {
+    id: string;
+    icon: string;
+    title: string;
+    date: Date;
+    tags: string[];
+    detail: string;
+    description: string;
+    content: CodeContentModel[];
+}
+
+export interface CodeContentModel {
+    version: PokeVersion;
+    blocks: CodeBlockModel[];
+}
+
+export interface CodeBlockModel {
+    title: string;
+    code: string;
+}
