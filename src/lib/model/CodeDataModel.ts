298eacc393674dd1f04cff2a4aed82a440643aaf
i-X-ce <iceci2804@gmail.com>
2025-10-11 03:09:29 +0900

    🎨 やっぱりスペースは2にした

--- src/lib/model/CodeDataModel.ts
+++ src/lib/model/CodeDataModel.ts
@@ -1,22 +1,22 @@
-import PokeVersion from "./pokeVersion";
+import PokeVersion from "./PokeVersion";
 
 export interface CodeDataModel {
-    id: string;
-    icon: string;
-    title: string;
-    date: Date;
-    tags: string[];
-    detail: string;
-    description: string;
-    content: CodeContentModel[];
+  id: string;
+  icon: string;
+  title: string;
+  date: Date;
+  tags: string[];
+  detail: string;
+  description: string;
+  content: CodeContentModel[];
 }
 
 export interface CodeContentModel {
-    version: PokeVersion;
-    blocks: CodeBlockModel[];
+  version: PokeVersion;
+  blocks: CodeBlockModel[];
 }
 
 export interface CodeBlockModel {
-    title: string;
-    code: string;
+  title: string;
+  code: string;
 }
