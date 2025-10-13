a61edc547bc58fd87225f22d0ae1df4fbea24891
i-X-ce <iceci2804@gmail.com>
2025-10-14 02:13:56 +0900

    🤡 モックデータの作成を行う関数

--- src/lib/model/CodeDataModel.ts
+++ src/lib/model/CodeDataModel.ts
@@ -1,4 +1,4 @@
-import PokeVersion from "./PokeVersion";
+import { PokeVersionType } from "./PokeVersion";
 
 export interface CodeDataModel {
   id: string;
@@ -12,11 +12,45 @@ export interface CodeDataModel {
 }
 
 export interface CodeContentModel {
-  version: PokeVersion;
+  version: PokeVersionType;
   blocks: CodeBlockModel[];
 }
 
 export interface CodeBlockModel {
   title: string;
   code: string;
+}
+
+export function createMockCodeData(num: number): CodeDataModel {
+  return {
+    id: `mock-id-${num}`,
+    icon: "🤡",
+    title: "セレクトバグ修正",
+    date: new Date(),
+    tags: ["ツール", "ゲーム", "攻略", "バグ", "裏技", "便利"],
+    detail:
+      "HRAMとマップスクリプトを併用して任意コード実行を行い、世界最大のバグであるセレクトバグを修正するコードです。コードによって何とかする頑張るよ。",
+    description:
+      "### すっごい頑張る  すっごい頑張ることで何とかするよ。  1. 頑張る  2. もっと頑張る  3. さらに頑張る",
+    content: [
+      {
+        version: "r0",
+        blocks: [
+          {
+            title: "セットアップ",
+            code: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+          },
+        ],
+      },
+      {
+        version: "g1",
+        blocks: [
+          {
+            title: "実行",
+            code: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+          },
+        ],
+      },
+    ],
+  };
 }
