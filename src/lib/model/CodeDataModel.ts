93ed1fc4e0b8f91940b966b3dd59ebe1f1a6cfe7
i-X-ce <iceci2804@gmail.com>
2025-10-15 23:04:50 +0900

    🤡 モックデータをランダムに

--- src/lib/model/CodeDataModel.ts
+++ src/lib/model/CodeDataModel.ts
@@ -1,4 +1,4 @@
-import { PokeVersionType } from "./PokeVersion";
+import { PokeVersions, PokeVersionType } from "./PokeVersion";
 
 export interface CodeDataModel {
   id: string;
@@ -22,35 +22,31 @@ export interface CodeBlockModel {
 }
 
 export function createMockCodeData(num: number): CodeDataModel {
+  const randomVersion = (): PokeVersionType => {
+    const versions: PokeVersionType[] = Object.values(PokeVersions);
+    return versions[Math.floor(Math.random() * versions.length)];
+  };
+
   return {
     id: `mock-id-${num}`,
     icon: "🤡",
-    title: "セレクトバグ修正",
+    title: `セレクトバグ修正-${num}`,
     date: new Date(),
     tags: ["ツール", "ゲーム", "攻略", "バグ", "裏技", "便利"],
     detail:
       "HRAMとマップスクリプトを併用して任意コード実行を行い、世界最大のバグであるセレクトバグを修正するコードです。コードによって何とかする頑張るよ。",
     description:
       "### すっごい頑張る  すっごい頑張ることで何とかするよ。  1. 頑張る  2. もっと頑張る  3. さらに頑張る",
-    content: [
-      {
-        version: "r0",
-        blocks: [
-          {
-            title: "セットアップ",
-            code: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
-          },
-        ],
-      },
-      {
-        version: "g1",
-        blocks: [
-          {
-            title: "実行",
-            code: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
-          },
-        ],
-      },
-    ],
+    content: [...Array(Math.floor(Math.random() * 5) + 1)].map((_, i) => ({
+      version: randomVersion(),
+      blocks: [
+        {
+          title: `コードブロック${i + 1}`,
+          code: Array((Math.floor(Math.random() * 10) + 1) * 5)
+            .fill(Math.random().toString(16).slice(2, 4).toUpperCase())
+            .join(""),
+        },
+      ],
+    })),
   };
 }
