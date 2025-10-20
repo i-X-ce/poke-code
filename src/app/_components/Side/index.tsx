96dea1e9753b4892b7ee03e6faa4362d3a7a4319
i-X-ce <iceci2804@gmail.com>
2025-10-21 03:42:18 +0900

    ✨ 新規作成ボタンを追加

--- src/app/_components/Side/index.tsx
+++ src/app/_components/Side/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './styles.module.css'
-import { Stack } from '@mui/material'
-import { AutoAwesome, Favorite, Home } from '@mui/icons-material'
+import { Button, Stack } from '@mui/material'
+import { Add, AutoAwesome, Favorite, Home } from '@mui/icons-material'
 import SideItem from './SideItem'
 import { createMockCodeData } from '@/lib/model/CodeDataModel'
 import SideItemChild from './SideItemChild'
+import DevelopmentComponent from '../DevelopmentComponent'
 
 const getCodeData = () => {
     return Array.from({ length: 5 }).map((_, i) => createMockCodeData(i))
@@ -28,6 +29,11 @@ function Side() {
                     </SideItem>
                 </Stack>
             </div>
+            <DevelopmentComponent>
+                <Button endIcon={<Add />} fullWidth variant='contained' sx={{ marginTop: 2 }}>
+                    新規作成
+                </Button>
+            </DevelopmentComponent>
         </div>
     )
 }
