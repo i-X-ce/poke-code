77c9a2fb9b4f5bd1fd779893a1414c6b010d7e88
i-X-ce <iceci2804@gmail.com>
2025-10-22 01:33:58 +0900

    💄 カードのスタイルを整える

---
+++ src/app/(view)/_components/CodeCard/index.tsx
@@ -0,0 +1,60 @@
+import { CodeDataModel } from '@/lib/model/CodeDataModel'
+import { PokeVersions } from '@/lib/model/PokeVersion'
+import { FavoriteBorder, MoreVert } from '@mui/icons-material'
+import { Box, Card, CardActionArea, CardContent, Chip, IconButton, Stack, Typography } from '@mui/material'
+import React from 'react'
+import VersionChip from '../VersionChip'
+import CodeInfo from '@/app/_components/CodeInfo'
+import styles from './styles.module.css'
+import DevelopmentComponent from '@/app/_components/DevelopmentComponent'
+
+interface CodeCardProps {
+    data: CodeDataModel
+}
+
+const CodeCard: React.FC<CodeCardProps> = ({ data }) => {
+    return (
+        <Card sx={{ position: "relative" }}>
+            <CardActionArea>
+                <CardContent>
+                    <Typography variant='h6' gutterBottom className={styles.title}>
+                        {/* <Typography component={"span"} fontSize={"1.8rem"}>{data.icon}</Typography> */}
+                        {data.icon} {data.title}
+                    </Typography>
+                    <Stack gap={1}>
+                        <CodeInfo data={data} />
+                        <Stack direction={"row"} gap={1}>
+                            {
+                                data.tags.map(tag => (
+                                    <Chip key={tag} label={tag} variant="outlined" />
+                                ))
+                            }
+                        </Stack>
+                        <Typography color="textSecondary" className={styles.detail}>
+                            {data.detail}
+                        </Typography>
+                        <Stack direction={"row"} spacing={1}>
+                            {
+                                Object.entries(PokeVersions).map(([_, version]) => (
+                                    <VersionChip key={version} version={version} disable={!data.content.some(c => c.version === version)} />
+                                ))
+                            }
+                        </Stack>
+                    </Stack>
+                </CardContent>
+            </CardActionArea>
+            <Stack direction={"row"} position={"absolute"} p={1} top={0} right={0}>
+                <IconButton>
+                    <FavoriteBorder />
+                </IconButton>
+                <DevelopmentComponent>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </DevelopmentComponent>
+            </Stack>
+        </Card>
+    )
+}
+
+export default CodeCard
