1c7458601ddb1c7d2a499ad4e3278037c72c4731
i-X-ce <iceci2804@gmail.com>
2025-10-28 00:51:26 +0900

    💄 ダークモード対応

--- src/app/_components/CustomMarkdown/index.tsx
+++ src/app/_components/CustomMarkdown/index.tsx
@@ -8,33 +8,33 @@ function CustomMarkdown({ children }: { children: string }) {
             components={{
                 h1: ({ children }) =>
                     <>
-                        <Typography variant='h3' fontWeight={"500"} paddingTop={6} gutterBottom>{children}</Typography>
+                        <Typography variant='h3' fontWeight={"500"} paddingTop={6} gutterBottom color='textPrimary'>{children}</Typography>
                         <Divider />
                     </>
                 ,
                 h2: ({ children }) =>
                     <>
-                        <Typography variant='h4' fontWeight={"500"} paddingTop={5} gutterBottom>{children}</Typography>
+                        <Typography variant='h4' fontWeight={"500"} paddingTop={5} gutterBottom color='textPrimary'>{children}</Typography>
                         <Divider />
                     </>
                 ,
                 h3: ({ children }) =>
                     <>
-                        <Typography variant='h5' fontWeight={"500"} paddingTop={3} gutterBottom>{children}</Typography>
+                        <Typography variant='h5' fontWeight={"500"} paddingTop={3} gutterBottom color='textPrimary'>{children}</Typography>
                         <Divider />
                     </>
                 ,
                 h4: ({ children }) =>
-                    <Typography variant='h6' paddingTop={2} gutterBottom>{children}</Typography>
+                    <Typography variant='h6' paddingTop={2} gutterBottom color='textPrimary'>{children}</Typography>
                 ,
                 h5: ({ children }) =>
-                    <Typography variant='h6' fontWeight={"400"} paddingTop={1} gutterBottom>{children}</Typography>
+                    <Typography variant='h6' fontWeight={"400"} paddingTop={1} gutterBottom color='textPrimary'>{children}</Typography>
                 ,
                 h6: ({ children }) =>
-                    <Typography variant='h6' fontWeight={"400"} paddingTop={1} gutterBottom>{children}</Typography>
+                    <Typography variant='h6' fontWeight={"400"} paddingTop={1} gutterBottom color='textPrimary'>{children}</Typography>
                 ,
                 p: ({ children }) =>
-                    <Typography variant='body1' gutterBottom>{children}</Typography>
+                    <Typography variant='body1' gutterBottom color='textPrimary'>{children}</Typography>
                 ,
                 a: ({ href, children }) =>
                     <Typography
@@ -58,7 +58,7 @@ function CustomMarkdown({ children }: { children: string }) {
                     <Typography component={"ol"} sx={{ paddingLeft: 3, paddingTop: 1, paddingBottom: 1 }}>{children}</Typography>
                 ,
                 li: ({ children }) =>
-                    <Typography variant='body1' component={"li"}>
+                    <Typography variant='body1' component={"li"} color='textPrimary'>
                         {children}
                     </Typography>
                 ,
@@ -71,7 +71,6 @@ function CustomMarkdown({ children }: { children: string }) {
                     return <Typography
                         component={"span"}
                         variant='body1'
-                        // color={isInline ? 'textSecondary' : 'textPrimary'}
                         color='textSecondary'
                         border={isInline ? '1px solid var(--bc-gray)' : 'none'}
                         sx={{
@@ -89,7 +88,6 @@ function CustomMarkdown({ children }: { children: string }) {
                         component={"pre"}
                         borderRadius={1}
                         sx={{
-                            // backgroundColor: "var(--bc-gray)",
                             boxShadow: "inset var(--sh-regular)",
                             padding: 2,
                         }}
