20590c5792e7696f66bd339e20872a9e73fbe97a
i-X-ce <iceci2804@gmail.com>
2025-10-29 01:08:00 +0900

    ✨ コードブロックにコピーボタンを追加

--- src/app/_components/CodeContentView/CodeBlock.tsx
+++ src/app/_components/CodeContentView/CodeBlock.tsx
@@ -1,17 +1,22 @@
+"use client"
+import useCopyClipboard from '@/lib/hooks/useCopyClipboard'
 import { CodeBlockModel } from '@/lib/model/CodeDataModel'
 import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import CopyButton from '../CopyButton'
 
 function CodeBlock({ block }: { block: CodeBlockModel }) {
+    const { handleCopy, copied } = useCopyClipboard(block.code);
+
     return (
-        <Stack>
+        <Stack position={"relative"} gap={1}>
             <Box
                 borderRadius={1}
                 py={.5}
                 px={2}
                 sx={{
-                    borderEndStartRadius: 0,
-                    borderEndEndRadius: 0,
+                    // borderEndStartRadius: 0,
+                    // borderEndEndRadius: 0,
                     backgroundColor: theme => theme.palette.text.disabled,
                     alignSelf: "start"
                 }}
@@ -24,10 +29,15 @@ function CodeBlock({ block }: { block: CodeBlockModel }) {
                 sx={{
                     border: theme => `1px ${theme.palette.divider} solid`,
                     borderRadius: 1,
-                    borderStartStartRadius: 0
+                    // borderStartStartRadius: 0
                 }}
             >
                 {block.code}
+            </Box>
+
+            {/* コピーボタン */}
+            <Box position={"absolute"} top={0} right={0}>
+                <CopyButton copied={copied} onClick={handleCopy} />
             </Box>
         </Stack>
     )
