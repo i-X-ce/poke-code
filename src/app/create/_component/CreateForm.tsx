16a5b976abf048e1ab0194146d3068ad23919477
i-X-ce <iceci2804@gmail.com>
2025-11-03 22:40:46 +0900

    🎨 プレースホルダーの追加

--- src/app/create/_component/CreateForm.tsx
+++ src/app/create/_component/CreateForm.tsx
@@ -3,7 +3,7 @@ import CodeContentView from '@/app/_components/CodeContentView';
 import VersionChip from '@/app/_components/VersionChip';
 import { CodeBlockModel, CodeContentModel, CodeDataModel } from '@/lib/model/CodeDataModel';
 import { PokeVersions, PokeVersionType } from '@/lib/model/PokeVersion';
-import { Delete, InsertEmoticon } from '@mui/icons-material';
+import { Add, Delete, InsertEmoticon } from '@mui/icons-material';
 import { Box, Button, Chip, IconButton, Popover, PopoverProps, Stack, TextField } from '@mui/material';
 import EmojiPicker from 'emoji-picker-react';
 import React from 'react'
@@ -18,14 +18,37 @@ const CODE_DATA_LABELS: { [K in keyof CodeDataModel]: string } = {
     detail: '概要',
     description: '説明(Markdown)',
     content: 'コード内容'
-};
+} as const;
 const CODE_BLOCK_LABELS: { [K in keyof CodeBlockModel]: string } = {
     title: 'タイトル',
     address: '開始アドレス',
     code: 'コード'
-};
+} as const;
+
+const CODE_DATA_PLACEHOLDERS: { [K in keyof CodeDataModel]: string } = {
+    id: '',
+    icon: '',
+    title: 'バイナリエディタ',
+    date: '',
+    tags: 'ツール、便利',
+    detail: 'バイナリエディタを起動するコードです。メモリを自由に書き換えることが可能になります。',
+    description: `## 使い方
+1. なかよしバッヂを入手する
+2. そだてやに「てEん」を預け、すぐに引き出す
+3. なかよしバッヂを使用する
+    `,
+    content: ''
+} as const;
+
+const CODE_BLOCK_PLACEHOLDERS: { [K in keyof CodeBlockModel]: string } = {
+    title: 'コードブロックのタイトル',
+    address: 'DA00',
+    code: '// ここにコードを記述します。\n00 01 02 03 04 05 06 07\n08 09 0A 0B 0C 0D 0E 0F'
+}
+
+const INIT_SELECTED_VERSION = PokeVersions.G1;
 const INIT_CODE_BLOCK: CodeBlockModel = { title: '', address: 'DA00', code: '' } as const;
-const INIT_CODE_CONTENT = (version: PokeVersionType): CodeContentModel => ({ version, blocks: [INIT_CODE_BLOCK] });
+const INIT_CODE_CONTENT = (version: PokeVersionType): CodeContentModel => ({ version, blocks: INIT_SELECTED_VERSION === version ? [INIT_CODE_BLOCK] : [] }) as const;
 
 const Date2String = (date: Date): string => date.toISOString().split('T')[0];
 
@@ -34,14 +57,12 @@ function CreateForm() {
         mode: "onChange",
         defaultValues: {
             date: Date2String(new Date()),
-            content: [INIT_CODE_CONTENT(PokeVersions.G1)]
+            content: Object.values(PokeVersions).map(v => INIT_CODE_CONTENT(v))
         }
     });
 
     const [emojiPickerAnchorEl, setEmojiPickerAnchorEl] = React.useState<HTMLElement | null>(null);
-    const [versionSelectorAnchorEl, setVersionSelectorAnchorEl] = React.useState<HTMLElement | null>(null);
-    const [selectedVersion, setSelectedVersion] = React.useState<PokeVersionType>(PokeVersions.G1);
-    const [enableSelectorAnchorEl, setEnableSelectorAnchorEl] = React.useState<HTMLElement | null>(null);
+    const [selectedVersion, setSelectedVersion] = React.useState<PokeVersionType>(INIT_SELECTED_VERSION);
 
     const handleEmojiPickerOpen = (event: React.MouseEvent<HTMLElement>) => {
         setEmojiPickerAnchorEl(event.currentTarget);
@@ -49,22 +70,6 @@ function CreateForm() {
 
     const handleEmojiPickerClose = () => {
         setEmojiPickerAnchorEl(null);
-    }
-
-    const handleVersionSelectorOpen = (event: React.MouseEvent<HTMLElement>) => {
-        setVersionSelectorAnchorEl(event.currentTarget);
-    }
-
-    const handleVersionSelectorClose = () => {
-        setVersionSelectorAnchorEl(null);
-    }
-
-    const handleEnableSelectorOpen = (event: React.MouseEvent<HTMLElement>) => {
-        setEnableSelectorAnchorEl(event.currentTarget);
-    }
-
-    const handleEnableSelectorClose = () => {
-        setEnableSelectorAnchorEl(null);
     }
 
     return (
@@ -89,16 +94,16 @@ function CreateForm() {
                         </>
                     )}
                 />
-                <TextField variant='filled' fullWidth label={CODE_DATA_LABELS.title} slotProps={{ inputLabel: { shrink: true } }} {...register("title", { required: "タイトルは必須です" })} error={!!errors.title?.message} helperText={errors.title?.message} />
+                <TextField variant='filled' fullWidth label={CODE_DATA_LABELS.title} placeholder={CODE_DATA_PLACEHOLDERS.title} slotProps={{ inputLabel: { shrink: true } }} {...register("title", { required: "タイトルは必須です" })} error={!!errors.title?.message} helperText={errors.title?.message} />
             </Stack>
             <Stack gap={2}>
                 <Stack direction={"row"} gap={2} flexWrap={'wrap'}>
                     <TextField fullWidth={false} type='date' label={CODE_DATA_LABELS.date} slotProps={{ inputLabel: { shrink: true } }}  {...register("date", { required: "日時は必須です" })} error={!!errors.date?.message} helperText={errors.date?.message} />
-                    <TextField sx={{ flex: 1 }} label={CODE_DATA_LABELS.tags} slotProps={{ inputLabel: { shrink: true } }} {...register("tags")} />
+                    <TextField sx={{ flex: 1 }} label={CODE_DATA_LABELS.tags} placeholder={CODE_DATA_PLACEHOLDERS.tags} slotProps={{ inputLabel: { shrink: true } }} {...register("tags")} />
                 </Stack>
-                <TextField multiline minRows={3} label={CODE_DATA_LABELS.detail} slotProps={{ inputLabel: { shrink: true } }} {...register("detail", { required: "概要は必須です" })} error={!!errors.detail?.message} helperText={errors.detail?.message} />
+                <TextField multiline minRows={3} label={CODE_DATA_LABELS.detail} placeholder={CODE_DATA_PLACEHOLDERS.detail} slotProps={{ inputLabel: { shrink: true } }} {...register("detail", { required: "概要は必須です" })} error={!!errors.detail?.message} helperText={errors.detail?.message} />
             </Stack>
-            <TextField multiline minRows={10} label={CODE_DATA_LABELS.description} slotProps={{ inputLabel: { shrink: true } }} {...register("description", { required: "説明は必須です" })} error={!!errors.description?.message} helperText={errors.description?.message} />
+            <TextField multiline minRows={10} label={CODE_DATA_LABELS.description} placeholder={CODE_DATA_PLACEHOLDERS.description} slotProps={{ inputLabel: { shrink: true } }} {...register("description", { required: "説明は必須です" })} error={!!errors.description?.message} helperText={errors.description?.message} />
 
             <Controller
                 name='content'
@@ -110,8 +115,6 @@ function CreateForm() {
                             content={field.value}
                             mode="edit"
                             selectedVersion={selectedVersion}
-                            onAdd={handleEnableSelectorOpen}
-                            onClick={handleVersionSelectorOpen}
                             onChangeVersion={(_, v) => setSelectedVersion(v)}
                         >
                             {
@@ -128,6 +131,7 @@ function CreateForm() {
                                             <TextField
                                                 value={b.title}
                                                 label={CODE_BLOCK_LABELS.title}
+                                                placeholder={CODE_BLOCK_PLACEHOLDERS.title}
                                                 slotProps={{ inputLabel: { shrink: true } }}
                                                 onChange={(e) => {
                                                     const newBlocks = [...field.value.find(c => c.version === selectedVersion)!.blocks];
@@ -139,12 +143,14 @@ function CreateForm() {
                                             <TextField
                                                 value={b.address}
                                                 label={CODE_BLOCK_LABELS.address}
+                                                placeholder={CODE_BLOCK_PLACEHOLDERS.address}
                                                 slotProps={{ inputLabel: { shrink: true } }}
                                             />
                                         </Stack>
                                         <TextField
                                             value={b.code}
                                             label={CODE_BLOCK_LABELS.code}
+                                            placeholder={CODE_BLOCK_PLACEHOLDERS.code}
                                             slotProps={{ inputLabel: { shrink: true } }}
                                             fullWidth
                                             multiline
@@ -171,7 +177,7 @@ function CreateForm() {
                                 bottom={0}
                             >
                                 <Chip
-                                    label="ブロック追加"
+                                    label="コードブロックを追加 +"
                                     variant='outlined'
                                     clickable
                                     color='primary'
@@ -187,27 +193,6 @@ function CreateForm() {
                                 />
                             </Box>
                         </CodeContentView>
-
-                        {/* バージョン追加用 */}
-                        <VersionsSelectorPopover
-                            enableVersions={field.value.map(c => c.version)}
-                            open={Boolean(enableSelectorAnchorEl)}
-                            anchorEl={enableSelectorAnchorEl}
-                            onClickVersion={(version) => {
-                                if (field.value.some(c => c.version === version)) return;
-                                field.onChange([...field.value, INIT_CODE_CONTENT(version)]);
-                                setSelectedVersion(version);
-                            }}
-                            onClose={handleEnableSelectorClose}
-                        />
-
-                        {/* バージョン入れ替え用 */}
-                        <VersionsSelectorPopover
-                            enableVersions={[field.value.find(c => c.version === selectedVersion)!.version]}
-                            open={Boolean(versionSelectorAnchorEl)}
-                            anchorEl={versionSelectorAnchorEl}
-                            onClose={handleVersionSelectorClose}
-                        />
                     </>
                 )
                 }
