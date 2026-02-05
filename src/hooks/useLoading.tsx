fe172152f1c043b789a4b38f3be595a8d45590f7
i-X-ce <iceci2804@gmail.com>
2026-02-05 17:47:39 +0900

    🎨 useLoadingのコールバックで返り値をちゃんと返すように変更

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 export const useLoading = () => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const startLoading = async (callback: () => Promise<void>) => {
+  const startLoading = async (callback: Function) => {
     try {
       setIsLoading(true);
-      await callback();
+      return await callback();
     } catch (error) {
       throw error;
     } finally {
