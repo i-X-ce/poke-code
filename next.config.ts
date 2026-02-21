579d74df332b779ce11a1da500cb075e66386e3b
i-X-ce <iceci2804@gmail.com>
2026-02-21 18:52:01 +0900

    🐛 画像のパスを修正したい

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ import { BASE_PATH } from "@/lib/constant/userSetting";
 import isDevelopment from "@/lib/util/isDevelopment";
 import type { NextConfig } from "next";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   pageExtensions: ["tsx", "ts", "jsx", "js"],
-  basePath: isDevelopment ? undefined : BASE_PATH,
-  assetPrefix: isDevelopment ? undefined : `${BASE_PATH}/`,
+  basePath: isDevelopment || isGitHubActions ? undefined : BASE_PATH,
+  assetPrefix: isDevelopment || isGitHubActions ? undefined : `${BASE_PATH}/`,
 };
 
 if (isDevelopment) {
