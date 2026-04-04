3f93415960c65d2981a1ee89789c87e1f6c99401
i-X-ce <iceci2804@gmail.com>
2026-04-05 03:54:28 +0900

    🚸 バージョン名がわかるようにツールチップを追加

--- src/components/VersionChip/index.tsx
+++ src/components/VersionChip/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { PokeVersionType } from "@/lib/types/PokeVersion";
+import { PokeVersionNames, PokeVersionType } from "@/lib/types/PokeVersion";
 import ver2css from "@/lib/util/str2css";
-import { Box, CardActionArea, useTheme } from "@mui/material";
+import { Box, CardActionArea, Tooltip } from "@mui/material";
 
 function VersionChip({
   version,
@@ -13,22 +13,26 @@ function VersionChip({
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }) {
   const content = (
-    <Box
-      borderRadius={1}
-      color={(theme) => theme.palette.background.paper}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: { xs: "1.2rem", sm: "1.5rem" },
-        aspectRatio: "1 / 1",
-        fontSize: { xs: "10.5px", sm: "12px" },
-        fontFamily: "var(--font-zen-kaku-gothic-new)",
-        backgroundColor: (theme) =>
-          disabled ? theme.palette.action.disabledBackground : ver2css(version),
-      }}>
-      {version.toUpperCase()}
-    </Box>
+    <Tooltip title={PokeVersionNames[version]} arrow>
+      <Box
+        borderRadius={1}
+        color={(theme) => theme.palette.background.paper}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: { xs: "1.2rem", sm: "1.5rem" },
+          aspectRatio: "1 / 1",
+          fontSize: { xs: "10.5px", sm: "12px" },
+          fontFamily: "var(--font-zen-kaku-gothic-new)",
+          backgroundColor: (theme) =>
+            disabled
+              ? theme.palette.action.disabledBackground
+              : ver2css(version),
+        }}>
+        {version.toUpperCase()}
+      </Box>
+    </Tooltip>
   );
 
   if (onClick) {
